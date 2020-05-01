7b6fbc9bdec32ad34fe936c589aea71e8d9f3ffb
Muhammad Farhan <farhanshah.96930012@gmail.com>
2020-05-01 14:01:21 +0530

    added cors config, helmet, compression for prod

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 require('express-async-errors');
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
+require('./startup/prod')(app);
 
 const server = app.listen(
   PORT,
