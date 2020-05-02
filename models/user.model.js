d14499ee499a2ed5870e57c4f1ff21958b7af0f6
Muhammad Farhan <farhanshah.96930012@gmail.com>
2020-05-02 08:06:18 +0530

    config key fixed

--- models/user.model.js
+++ models/user.model.js
@@ -27,7 +27,7 @@ const userSchema = mongoose.Schema({
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
-    { _id: this._id, isAdmin: this.isAdmin },
+    { _id: this._id },
     config.get('jwtPrivateKey')
   );
   return token;
